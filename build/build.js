76d3197b6e5b0b639e2da7a07453ce6586758735
Rajat Ninawe <rajatninawe123@gmail.com>
2025-01-07 20:01:32 +0530

    updated build setup

--- build/build.js
+++ build/build.js
@@ -1,13 +1,26 @@
-// build.js
 const esbuild = require('esbuild');
-// const { createOptions } = require('esbuild-plugin-ts');
+const { readFileSync, writeFileSync } = require('fs');
+const { brotliCompressSync } = require('zlib');
 
+// Build configuration
 esbuild
   .build({
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    outfile: 'dist/bundle.js',
-    minify: true,
-    // plugins: [createOptions()],
+    entryPoints: ['src/index.ts'], // Entry point for your application
+    bundle: true, // Bundle all dependencies
+    minify: true, // Minify the output files
+    splitting: true, // Enable code splitting
+    format: 'esm', // Output format (ESM)
+    outdir: 'dist', // Output directory
+    sourcemap: true, // Generate sourcemaps
+    metafile: true, // Generate metafile for analysis
+  })
+  .then(() => {
+    // Optionally compress files with Brotli
+    const files = ['dist/index.js'];
+    files.forEach((file) => {
+      const contents = readFileSync(file);
+      const compressed = brotliCompressSync(contents);
+      writeFileSync(`${file}.br`, compressed);
+    });
   })
   .catch(() => process.exit(1));
