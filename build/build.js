e20d4c979410159c468d05272308104710dd8d47
Rajat Ninawe <rajatninawe123@gmail.com>
2025-02-18 20:16:27 +0530

    Added compatibility for all envs

--- build/build.js
+++ build/build.js
@@ -6,7 +6,7 @@ const { execSync } = require('child_process');
 // Generate Type Definitions using TypeScript compiler
 execSync('tsc --declaration --emitDeclarationOnly --outDir dist/types');
 
-// Build configuration
+// Build configuration for ESM
 esbuild
   .build({
     entryPoints: ['src/index.ts'], // Entry point for your application
@@ -14,13 +14,36 @@ esbuild
     minify: true, // Minify the output files
     splitting: true, // Enable code splitting
     format: 'esm', // Output format (ESM)
-    outdir: 'dist', // Output directory
+    outdir: 'dist/esm', // Output directory for ESM
     sourcemap: true, // Generate sourcemaps
     metafile: true, // Generate metafile for analysis
   })
   .then(() => {
     // Optionally compress files with Brotli
-    const files = ['dist/index.js'];
+    const files = ['dist/esm/index.js'];
+    files.forEach((file) => {
+      const contents = readFileSync(file);
+      const compressed = brotliCompressSync(contents);
+      writeFileSync(`${file}.br`, compressed);
+    });
+  })
+  .catch(() => process.exit(1));
+
+// Build configuration for CommonJS
+esbuild
+  .build({
+    entryPoints: ['src/index.ts'], // Entry point for your application
+    bundle: true, // Bundle all dependencies
+    minify: true, // Minify the output files
+    splitting: false, // Disable code splitting for CommonJS
+    format: 'cjs', // Output format (CommonJS)
+    outdir: 'dist/cjs', // Output directory for CommonJS
+    sourcemap: true, // Generate sourcemaps
+    metafile: true, // Generate metafile for analysis
+  })
+  .then(() => {
+    // Optionally compress files with Brotli
+    const files = ['dist/cjs/index.js'];
     files.forEach((file) => {
       const contents = readFileSync(file);
       const compressed = brotliCompressSync(contents);
