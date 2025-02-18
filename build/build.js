d9b0e203ae61fae9f53b8a93491321863026c4c8
Rajat Ninawe <rajatninawe123@gmail.com>
2025-02-18 22:35:20 +0530

    Updated build compatibility

--- build/build.js
+++ build/build.js
@@ -1,7 +1,7 @@
-const esbuild = require('esbuild');
-const { readFileSync, writeFileSync } = require('fs');
-const { brotliCompressSync } = require('zlib');
-const { execSync } = require('child_process');
+import esbuild from 'esbuild';
+import { readFileSync, writeFileSync } from 'fs';
+import { brotliCompressSync } from 'zlib';
+import { execSync } from 'child_process';
 
 // Generate Type Definitions using TypeScript compiler
 execSync('tsc --declaration --emitDeclarationOnly --outDir dist/types');
@@ -14,36 +14,13 @@ esbuild
     minify: true, // Minify the output files
     splitting: true, // Enable code splitting
     format: 'esm', // Output format (ESM)
-    outdir: 'dist/esm', // Output directory for ESM
+    outdir: 'dist', // Output directory for ESM
     sourcemap: true, // Generate sourcemaps
     metafile: true, // Generate metafile for analysis
   })
   .then(() => {
     // Optionally compress files with Brotli
-    const files = ['dist/esm/index.js'];
-    files.forEach((file) => {
-      const contents = readFileSync(file);
-      const compressed = brotliCompressSync(contents);
-      writeFileSync(`${file}.br`, compressed);
-    });
-  })
-  .catch(() => process.exit(1));
-
-// Build configuration for CommonJS
-esbuild
-  .build({
-    entryPoints: ['src/index.ts'], // Entry point for your application
-    bundle: true, // Bundle all dependencies
-    minify: true, // Minify the output files
-    splitting: false, // Disable code splitting for CommonJS
-    format: 'cjs', // Output format (CommonJS)
-    outdir: 'dist/cjs', // Output directory for CommonJS
-    sourcemap: true, // Generate sourcemaps
-    metafile: true, // Generate metafile for analysis
-  })
-  .then(() => {
-    // Optionally compress files with Brotli
-    const files = ['dist/cjs/index.js'];
+    const files = ['dist/index.js'];
     files.forEach((file) => {
       const contents = readFileSync(file);
       const compressed = brotliCompressSync(contents);
