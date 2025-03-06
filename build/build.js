96db629f6338a81ee5dd7bc4048c073dbcc2d502
Rajat Ninawe <rajatninawe123@gmail.com>
2025-03-06 19:53:36 +0530

    Updated build generation settings

--- build/build.js
+++ build/build.js
@@ -3,28 +3,55 @@ import { readFileSync, writeFileSync } from 'fs';
 import { brotliCompressSync } from 'zlib';
 import { execSync } from 'child_process';
 
-// Generate Type Definitions using TypeScript compiler
-execSync('tsc --declaration --emitDeclarationOnly --outDir dist/types');
+async function build() {
+  try {
+    // Generate Type Definitions using TypeScript compiler
+    execSync('tsc --declaration --emitDeclarationOnly --outDir dist/types');
 
-// Build configuration for ESM
-esbuild
-  .build({
-    entryPoints: ['src/index.ts'], // Entry point for your application
-    bundle: true, // Bundle all dependencies
-    minify: true, // Minify the output files
-    splitting: true, // Enable code splitting
-    format: 'esm', // Output format (ESM)
-    outdir: 'dist', // Output directory for ESM
-    sourcemap: true, // Generate sourcemaps
-    metafile: true, // Generate metafile for analysis
-  })
-  .then(() => {
+    const esbuildOptions = {
+      entryPoints: ['src/index.ts'],
+      bundle: true,
+      minify: true,
+      splitting: true,
+      sourcemap: 'external', // Generate external sourcemaps
+      sourcesContent: false, // Omit original source code from sourcemaps
+      metafile: true,
+    };
+
+    // Build configuration for ESM
+    await esbuild.build({
+      ...esbuildOptions,
+      format: 'esm',
+      outdir: 'dist/esm', // Separate ESM output directory
+    });
+
+    // Build configuration for CJS
+    await esbuild.build({
+      ...esbuildOptions,
+      splitting: false,
+      format: 'cjs',
+      outdir: 'dist/cjs', // Separate CJS output directory
+    });
+
     // Optionally compress files with Brotli
-    const files = ['dist/index.js'];
-    files.forEach((file) => {
+    const filesToCompress = [
+      'dist/esm/index.js',
+      'dist/esm/index.js.map',
+      'dist/cjs/index.js',
+      'dist/cjs/index.js.map',
+    ];
+
+    filesToCompress.forEach((file) => {
       const contents = readFileSync(file);
       const compressed = brotliCompressSync(contents);
       writeFileSync(`${file}.br`, compressed);
     });
-  })
-  .catch(() => process.exit(1));
+
+    console.log('Build completed successfully.');
+  } catch (error) {
+    console.error('Build failed:', error);
+    process.exit(1);
+  }
+}
+
+build();
